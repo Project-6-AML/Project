2375482f39c4261d018cb234c32813eeb5573a17
alemsc99 <116645532+alemsc99@users.noreply.github.com>
2023-02-12 12:53:24 +0100

    prova 3 AL

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ def test(args: Namespace, eval_ds: Dataset, model: torch.nn.Module) -> Tuple[np.
                                          batch_size=args.infer_batch_size, pin_memory=(args.device == "cuda"))
         print(args.fc_output_dim)
         all_descriptors = np.empty((len(eval_ds), args.fc_output_dim), dtype="float32")
-        all_descriptors = np.empty((len(eval_ds), features_dim), dtype="float32")
+        #all_descriptors = np.empty((len(eval_ds), features_dim), dtype="float32")
         for images, indices in tqdm(database_dataloader, ncols=100):
             #descriptors = model(images.to(args.device))
             #descriptors = descriptors.cpu().numpy()
